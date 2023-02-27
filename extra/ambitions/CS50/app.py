fbd91ed6e0c34a0c25f8ac595f7535b5414ee2c4
Boris Depoortere <boris.depoortere@student.howest.be>
2023-02-27 09:35:00 +0100

    Auto Server Commit 27.02.2023 at 09:35

--- extra/ambitions/CS50/app.py
+++ extra/ambitions/CS50/app.py
@@ -1,11 +1,32 @@
+#controller
+#view: jinja, html
+
+import os
+
 from flask import Flask, render_template,request
+from flask_mail import Mail, Message
+
+
 
 app = Flask(__name__)
+
+mail = Mail(app)
 
 #decorator (special funct that modifies other funct)
 
 # define a route for /
 @app.route("/")
+
+#app.config["MAIL_DEFAULT_SENDER"] = os.environ["MAIL_DEFAULT_SENDER"]
+#app.config["MAIL_DEFAULT_SENDER"] = 
+
+def index():
+
+    msg = Message("Hello",
+              recipients=["boris.depoortere69@gmail.com"])
+    msg.body = "testing"
+    msg.html = "<b>testing</b>"
+    mail.send(msg)
 
 #just return render template of index.html
 
